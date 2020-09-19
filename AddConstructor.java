4e67a319c55715ea117b1b0890f135578aaa578a
Vadim-cpu <68536347+Vadim-cpu@users.noreply.github.com>
2020-09-19 23:00:39 +0300

    Add files via upload

--- AddConstructor.java
+++ AddConstructor.java
@@ -1,13 +1,5 @@
-import static java.lang.annotation.ElementType.TYPE;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
+package original;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
-
-@Documented
-@Retention(RUNTIME)
-@Target(TYPE)
 public @interface AddConstructor {
 
 }
